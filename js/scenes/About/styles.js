9d950f3da4b20d20188e3c09fb3d5346532fb019
tmclean85 <trevor.a.mclean@gmail.com>
2017-08-22 22:14:41 -0700

    Style about page

--- js/scenes/About/styles.js
+++ js/scenes/About/styles.js
@@ -1,29 +1,41 @@
 import { StyleSheet } from 'react-native';
-
+import { colors, typography } from '../../config/styles';
 
 export const styles = StyleSheet.create({
-  headerContainer: {
+  container: {
     flex: 1,
-    height: 50,
-    width: 225,
+    margin: 10,
+    marginTop: 20,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    margin: 30,
     justifyContent: 'center',
-    borderBottomWidth: 1,
-    borderColor:'black'
+    alignItems: 'center'
   },
   headerImage: {
-    height: 50,
-    width: 200,
-    marginTop: 150
-
+    flexDirection: 'row',
+    margin: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   headerInfo: {
     height: 50
   },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    alignItems: 'flex-start',
+    margin: 10,
+    fontFamily: typography.fontMain
+  },
+
+  headerText: {
+    fontWeight: 'bold',
+    color: '#9963ea',
+    fontFamily: typography.fontMain
+  },  
   list: {
     marginTop: 100,
   },
-  codeItem: {
-    marginTop: 25,
-    marginBottom: 25
-  }
 });
